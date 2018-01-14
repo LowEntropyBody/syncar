26a46792d25311a8074cf82f749a1744809bb0ad
LowEntropyBody <jszyyx@163.com>
2018-01-14 19:16:28 +0800

    Client -- modify RectTarget

--- synclient/syncar.cpp
+++ synclient/syncar.cpp
@@ -104,7 +104,7 @@ int main(int argc, char* argv[])
 	cout << "------target detection------" << endl;
 	// 旋转一圈寻找目标
 	for(int i = 0; i < 10; i++){
-		cout << " car base degree:" << rotate_degrees_array[9] << endl;
+		cout << " car base degree:" << rotate_degrees_array[i] << endl;
 		car.move_rotate(rotate_degrees_array[i]);
 		int takephoto_index = -1;
 		int find_num = 0;
@@ -258,7 +258,7 @@ double RectTarget::getDegree(){
 }
 // 输出信息
 void RectTarget::show(){
-	cout << " id: " << id << endl;
+	cout << endl << "//////////////////" << "id: " << id << endl;
 	if(cm == color_match_red) cout << " color: red" << endl;
 	if(cm == color_match_green) cout << " color: green" << endl;
 	cout << " width: " << width << endl;
