8761271e09c2950f2a8c83937d951a712f3a9029
LowEntropyBody <jszyyx@163.com>
2018-01-14 19:12:01 +0800

    Client -- modify RectTarget

--- synclient/syncar.cpp
+++ synclient/syncar.cpp
@@ -30,6 +30,7 @@ class RectTarget {
 		int cm_width;
 	public:
 		unsigned char* pic_rgb;
+		double base_degree;
 		RectTarget(string id_temp, int (*color_match_temp)(int,int,int), double width_temp, double height_temp);
 		~RectTarget();
 		bool findTarget(bool isSave, string flag);
@@ -38,6 +39,7 @@ class RectTarget {
 		double getDistance();
 		double getDegree();
 		void show();
+		bool isfind();
 };
 
 // 标识设备id
@@ -91,25 +93,39 @@ int main(int argc, char* argv[])
 	cout << " car motative system start" << endl;
 	rts.push_back(new RectTarget("target1", color_match_red, 8.2, 21));
 	rts.push_back(new RectTarget("target2", color_match_red, 5.6, 21));
-	rts.push_back(new RectTarget("target3", color_match_green, 8.2, 21));
-	rts.push_back(new RectTarget("target4", color_match_green, 5.6, 21));
-	rts.push_back(new RectTarget("target5", color_match_blue, 8.2, 21));
-	rts.push_back(new RectTarget("target6", color_match_blue, 5.6, 21));
-	cout << " target infornation load success" << endl;
+	//rts.push_back(new RectTarget("target3", color_match_green, 8.2, 21));
+	//rts.push_back(new RectTarget("target4", color_match_green, 5.6, 21));
+	//rts.push_back(new RectTarget("target5", color_match_blue, 8.2, 21));
+	//rts.push_back(new RectTarget("target6", color_match_blue, 5.6, 21));
+	cout << " target information load success" << endl;
+	// 旋转角度
+	int rotate_degrees_array[9] = {0,40,80,120,160,200,240,280,320};
 	
-	
-	/*car.move_rotate(100);
-	usleep(1000*2000);
-	car.move_rotate(0);
-	usleep(1000*2000);
-	car.move_rotate(-100);
-	usleep(1000*2000);
-	car.move_rotate(0);*/
-	rts[0]->findTarget(false, "0");
-	rts[0]->show();
-	
-	rts[1]->findTarget(rts[0]->pic_rgb, false, "0");
-	rts[1]->show();
+	cout << "------target detection------" << endl;
+	// 旋转一圈寻找目标
+	for(int i = 0; i < 10; i++){
+		cout << " car base degree:" << rotate_degrees_array[9] << endl;
+		car.move_rotate(rotate_degrees_array[i]);
+		int takephoto_index = -1;
+		int find_num = 0;
+		for(int j = 0; j < rts.size(); j++){
+			if(!rts[j]->isfind()){
+				if(takephoto_index != -1){
+					rts[j]->findTarget(rts[takephoto_index]->pic_rgb, false, "0");
+				}else{
+					takephoto_index = j;
+					rts[j]->findTarget(false, "0");
+				}
+				if(rts[j]->isfind()) rts[j]->base_degree = rotate_degrees_array[i];
+			}else find_num++;
+		}
+		if(find_num == rts.size()) break;
+		usleep(1000*1000);
+	}
+	// 归位
+	car.move_rotate(rotate_degrees_array[i]);
+	for(int i = 0; i < rts.size(); i++) rts.show();
+		
 	
 	cout << "------system end------" << endl;
 	return   0;
@@ -127,15 +143,16 @@ RectTarget::RectTarget(string id_temp, int (*color_match_temp)(int,int,int), dou
 	pic_rgb = NULL;
 	cm_width = 640;
 	cm_height = 320;
+	base_degree = 0;
 }
-//析构函数
+// 析构函数
 RectTarget::~RectTarget(){
 	if(infor != NULL) free(infor);
 	if(pic_rgb != NULL) free(pic_rgb);
 }
-//找目标返回是否找到目标
+// 找目标返回是否找到目标
 bool RectTarget::findTarget(bool isSave, string flag){
-	//打开摄像机
+	// 打开摄像机
 	camera_t* camera = camera_open("/dev/video0", cm_width, cm_height);
 	camera_init(camera);
 	camera_start(camera);
@@ -146,9 +163,9 @@ bool RectTarget::findTarget(bool isSave, string flag){
 	for (int i = 0; i < 5; i++) {
 		camera_frame(camera, timeout);
 	}
-	//拍摄照片
+	// 拍摄照片
 	camera_frame(camera, timeout);
-	//转换为rgb
+	// 转换为rgb
 	if(pic_rgb != NULL) free(pic_rgb);
 	pic_rgb = yuyv2rgb(camera->head.start, camera->width, camera->height);
 	if(isSave){
@@ -159,7 +176,7 @@ bool RectTarget::findTarget(bool isSave, string flag){
 		fclose(out);
 		cout << " save orign picture into " << name << endl;
 	}
-	//找目标
+	// 找目标
 	if(infor != NULL) free(infor);
 	infor = find_aim(pic_rgb, camera->width, camera->height, cm, width, height);
 	/*
@@ -185,6 +202,9 @@ bool RectTarget::findTarget(bool isSave, string flag){
 			double k = (double)(infor -> center_y)/(double)(320 - infor -> center_y);
 			degree = atan(1/((1+k)*sqrt(3)))*180.0/3.14159;
 		}
+		// 余弦定理算机器人中心距目标多远c^2 = a^2 + b^2 - 2abcos(<c)
+		distance = sqrt(14.5 * 14.5 + distance * distance -
+						2 * 14.5 * distance * cos((double)(180 - abs(degree))/180.0 * 3.1415926));
 	}
 	return 0;
 }
@@ -201,13 +221,12 @@ bool RectTarget::findTarget(unsigned char* rbg_temp, bool isSave, string flag){
 		fclose(out);
 		cout << " save orign picture into " << name << endl;
 	}
-	//找目标
+	// 找目标
 	if(infor != NULL) free(infor);
 	infor = find_aim(pic_rgb, cm_width, cm_height, cm, width, height);
 	if(infor -> isfind){
-		//摄像头到目标距离
+		// 摄像头到目标距离
 		distance = (double)(707.14 * height)/(double)infor -> l;
-		printf("dddddddddd:%f\n",distance);
 		degree = 0;
 		if(infor -> center_y > 320){
 			double k = (double)(640 - infor -> center_y)/(double)(infor -> center_y - 320);
@@ -216,25 +235,28 @@ bool RectTarget::findTarget(unsigned char* rbg_temp, bool isSave, string flag){
 			double k = (double)(infor -> center_y)/(double)(320 - infor -> center_y);
 			degree = atan(1/((1+k)*sqrt(3)))*180.0/3.1415926;
 		}
-		//余弦定理算机器人中心距目标多远c^2 = a^2 + b^2 - 2abcos(<c)
+		// 余弦定理算机器人中心距目标多远c^2 = a^2 + b^2 - 2abcos(<c)
 		distance = sqrt(14.5 * 14.5 + distance * distance -
 						2 * 14.5 * distance * cos((double)(180 - abs(degree))/180.0 * 3.1415926));
 	}
 	return 0;
 }
-
+bool RectTarget::isfind(){
+	if(infor == NULL) return false;
+	return infor -> isfind;
+}
 aim_infor* RectTarget::getAimInfor(){
 	return infor;
 }
-//返回目标距离
+// 返回目标距离
 double RectTarget::getDistance(){
 	return distance;
 }
-//返回目标偏角
+// 返回目标偏角
 double RectTarget::getDegree(){
 	return degree;
 }
-//输出信息
+// 输出信息
 void RectTarget::show(){
 	cout << " id: " << id << endl;
 	if(cm == color_match_red) cout << " color: red" << endl;
@@ -243,6 +265,7 @@ void RectTarget::show(){
 	cout << " height: " << height << endl;
 	cout << " distance: " << distance << endl;
 	cout << " degree: " << degree << endl;
+	cout << " base degree: " << base_degree << endl;
 	if(infor != NULL){
 		if(infor->isfind){
 			cout << " infor->isfind: " << "yes" << endl;
