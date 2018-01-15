5edb88fa7e029cfaedd599ad0b51899d42dee059
LowEntropyBody <jszyyx@163.com>
2018-01-15 16:18:00 +0800

    Client-- add move to aim function

--- synclient/syncar.cpp
+++ synclient/syncar.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[])
 	cout << endl << "------target detection------" << endl;
 	// 旋转一圈寻找目标
 	for(int i = 0; i < 9; i++){
-		cout << " car base degree:" << rotate_degrees_array[i] << endl;
+		cout << " car base degree: " << rotate_degrees_array[i] << endl;
 		car.move_rotate(rotate_degrees_array[i]);
 		int takephoto_index = -1;
 		int find_num = 0;
@@ -103,6 +103,7 @@ int main(int argc, char* argv[])
 				if(rts[j]->isfind()){
 					rts[j]->base_degree = rotate_degrees_array[i];
 					find_num++;
+					cout << "     find target: " << rts[j]->id << endl;
 				}
 			}
 		}
