c51b40c3689034be453910fd0e6ae388f0470047
LowEntropyBody <jszyyx@163.com>
2018-02-28 19:21:57 +0800

    Client -- test three cameras

--- synclient/client_dynamic/main.cpp
+++ synclient/client_dynamic/main.cpp
@@ -13,6 +13,13 @@
 #include <vector>
 
 int main(int argc, char* argv[]){
+	Cam* cam1 = new Cam("/dev/video0", 640, 360);
+	Cam* cam2 = new Cam("/dev/video1", 640, 360);
+	cam1 -> take_pic();
+	cam1 -> save_pic("re1.jpg");
+	cam2 -> take_pic();
+	cam2 -> save_pic("re2.jpg"); 
+	/**
 	ThreadCam* tc1 = new ThreadCam("/dev/video0", 640, 360);
 	ThreadCam* tc2 = new ThreadCam("/dev/video1", 640, 360);
 	tc1->thread_run();
@@ -37,5 +44,6 @@ int main(int argc, char* argv[]){
 	tc1->thread_stop();
 	tc2->thread_stop();
 	cout << "main" << endl;
+	**/
 	return 0;
 }
