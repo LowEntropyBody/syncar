439c759d6bea25648574b9af90c27f2136b5274d
LowEntropyBody <jszyyx@163.com>
2018-03-02 14:30:45 +0800

    Client -- test car

--- synclient/client_dynamic/main.cpp
+++ synclient/client_dynamic/main.cpp
@@ -15,15 +15,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+	
+
+	
+ 	cout << "--------------------------START" << endl;
 	CarSpeed* cs = new CarSpeed(); 
 	cs -> move_frist_start();
-	cs -> move(30,100,20);
-	usleep(1000*2000);
-	cs -> speed_x_y_z(0,0,0);
-	/**
- 	cout << "--------------------------START" << endl;
-	Car* car = new Car();
-	car -> move_frist_start();
 	
 	vector<ThreadCam*> cams;
 	cams.push_back(new ThreadCam("/dev/video0", 640, 360, -35));
@@ -33,30 +30,30 @@ int main(int argc, char* argv[]){
 		cams[i] -> thread_run();
 	
 	int dd = 0;
-	while(dd < 1000){
+	while(dd < 500){
 		MoveInfo* move_info;
 		for(int i = 0; i < cams.size(); i++){
 			move_info = cams[i] -> get_move_info();
 			if(move_info != NULL){
 				cout << endl << "-----Cam Index:" << i + 1 << "------" << endl;
-				cout << "now_degree:" << car -> now_degree << endl;
-				cout << "base_degree:" << move_info -> base_degree << endl;
-				cout << "degree:" << move_info -> degree << endl;
-				car -> move_rotate(car -> now_degree + move_info -> base_degree + move_info -> degree);
-				car -> move_forward(move_info -> distance/2);
+				cs -> move(move_info -> base_degree + move_info -> degree, move_info -> distance, 20);
 				cams[i] -> show();
 				break;
 			}
 		}
-		usleep(1000*100);
+		usleep(1000*200);
 		dd ++;
-		if(dd%10 == 0) cout << "This is " << dd/10 << "s" << endl;
+		if(dd%5 == 0) { 
+			cs -> speed_x_y_z(0, 0, 0);
+			cout << "This is " << dd/5 << "s" << endl;
+		}
 	}
+	cs -> speed_x_y_z(0, 0, 0);
 	//usleep(1000*10000);
 	for(int i = 0; i < cams.size(); i++) 
 		cams[i] -> thread_stop();
 	cout << "-------------------------END" << endl;
-	**/
+	
 	
 	return 0;
 }
